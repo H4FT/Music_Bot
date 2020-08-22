05a90e857f8217aaee1d919ad46302d224ca4bf1
Arthur Azibi <azibiarthur@gmail.com>
2020-08-22 14:47:48 +0200

    [ADD] embed message for queue

--- src/list.js
+++ src/list.js
@@ -3,12 +3,24 @@ const error = require('./error_message');
 module.exports = {
     list: function list(message, serverQueue) {
         if (serverQueue) {
-            message.channel.send("**Music in queue:**");
-            for (let i = 0; serverQueue.songs[i]; i++) {
-                let song = serverQueue.songs[i];
-                message.channel.send(`${i} : **${song[0].title}**\n`);
+            let queue;
+            for (let b = 0; serverQueue.songs[b] != null; b++) {
+                if (queue === undefined) {
+                    queue = serverQueue.songs[b].map(result => {
+                        return `${b}: **__${result.title}__**\n`;
+                    });
+                } else {
+                    queue = queue + serverQueue.songs[b].map(result => {
+                        return `${b}: **__${result.title}__**\n`;
+                    });
+                }
             }
-            return;
+            return message.channel.send( {
+                embed: {
+                    title: "QUEUE",
+                    description: String(queue),
+                }
+            });
         } else {
             error.error(message, 1);
             return;
